ad6a6afa7c5e00bc14c1ea72e1941485de5d6187
TrevorSeward_cp <SND\TrevorSeward_cp>
2013-01-04 06:42:23 +0000

    Code for loading STS web.config.

---
+++ Nauplius.ADLDS.UserProfiles/Nauplius.ADLDS.FBA/Features/AdminPagesFeature/AdminPagesFeature.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="6f5b45ae-46d0-4d1e-9540-61c4f6026430" activateOnDefault="false" featureId="6f5b45ae-46d0-4d1e-9540-61c4f6026430" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="Nauplius.ADLDS.FBA Administration Pages" version="" autoActivateInCentralAdmin="true" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="c6d0e2a0-8079-4eaa-9fe5-227720692e71" />
+  </projectItems>
+</feature>
---
+++ Nauplius.ADLDS.UserProfiles/Nauplius.ADLDS.FBA/STSSyncMonitor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
+using Microsoft.SharePoint.Administration;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.Web.Administration;
+using Microsoft.SharePoint.Administration.Claims;
+using System.Xml;
+
+namespace Nauplius.ADLDS.FBA
+{
+    [Guid("DF4A0729-0577-4EFB-8C7E-857BBBDA5CCC")]
+    internal class STSSyncMonitor : SPJobDefinition
+    {
+        private const string tJobName = "Nauplius ADLDS FBA STS Sync Monitor";
+
+        public STSSyncMonitor() : base()
+        {
+        }
+
+        public STSSyncMonitor(String name, SPWebApplication adminWebApplication, SPServer server, SPJobLockType lockType)
+            : base(name, adminWebApplication, server, lockType)
+        {
+        }
+
+        public STSSyncMonitor(String name, SPWebApplication adminWebApplication)
+            : base(name, adminWebApplication, null, SPJobLockType.Job)
+        {
+            this.Title = tJobName;
+        }
+
+        public override void Execute(Guid targetInstanceId)
+        {
+            Logging.LogMessage(900, Logging.LogCategories.TimerJob, TraceSeverity.Medium, "Entering " + tJobName);
+            List<XmlDocument> stsConfigurations = new List<XmlDocument>();
+
+            foreach (SPServer spServer in SPFarm.Local.Servers)
+            {
+                string path = SPUtility.GetGenericSetupPath(@"WebServices\SecurityToken\web.config");
+                XmlDocument config = new XmlDocument();
+                config.Load(path);
+                stsConfigurations.Add(config);
+            }
+        }
+    }
+}
